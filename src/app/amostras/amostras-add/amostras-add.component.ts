ddcf960b725b30edc6a4503d6ae0d20113b4d4a0
kevinguimaraes <kevinsiob@hotmail.com>
2019-11-26 20:24:00 -0300

    amostra working

--- src/app/amostras/amostras-add/amostras-add.component.ts
+++ src/app/amostras/amostras-add/amostras-add.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Amostra } from 'app/model/amostra';
 import { AmostraService } from 'app/services/amostra.service';
+import { Cidade } from 'app/model/cidade';
+import { Estado } from 'app/model/estado';
+import { CidadeService } from 'app/services/cidade.service';
+import { EstadoService } from 'app/services/estado.service';
+import { ClienteService } from 'app/services/cliente.service';
+import { Cliente } from 'app/model/cliente';
 
 @Component({
   selector: 'app-amostras-add',
@@ -10,7 +16,20 @@ import { AmostraService } from 'app/services/amostra.service';
 })
 export class AmostrasAddComponent implements OnInit {
   amostra: Amostra;
-  constructor(private route: ActivatedRoute, private router: Router, private _api: AmostraService) { }
+  cidade: Cidade;
+  estado: Estado;
+  cliente: Cliente;
+  cidades: Cidade[];
+  estados: Estado[];
+  clientes: Cliente[];
+
+  constructor(private route: ActivatedRoute, 
+    private router: Router, 
+    private apiAmostra: AmostraService,
+    private apiCidade: CidadeService,
+    private apiEstado: EstadoService,
+    private apiCliente: ClienteService
+    ) { }
 
   backInitial(){
     this.router.navigate(['/amostras']);
@@ -18,16 +37,27 @@ export class AmostrasAddComponent implements OnInit {
 
   ngOnInit() {
     this.amostra = new Amostra();
+    this.cidade = new Cidade();
+    this.estado = new Estado();
+    this.cliente = new Cliente();
+    this.apiCidade.getCidades()
+    .subscribe(res =>
+      this.cidades = res);
+    this.apiEstado.getEstados()
+    .subscribe(res =>
+      this.estados = res)
+      this.apiCliente.getClientes()
+    .subscribe(res =>
+      this.clientes = res)
+
   }
 
-  enviarAmostra(){
-    this.amostra.id = null;
-    this.amostra.cliente = null;
-    this.amostra.endereco.cidade = null;
+  enviarAmostra() {
+    this.amostra.cliente = this.cliente;
+    this.amostra.endereco.cidade = this.cidade;
 
-    this._api.addAmostra(this.amostra);
-    
-      this._api.addAmostra(this.amostra)
+    this.apiAmostra.addAmostra(this.amostra);
+      this.apiAmostra.addAmostra(this.amostra)
         .subscribe(res => {
             console.log(res);
           }, (err) => {
